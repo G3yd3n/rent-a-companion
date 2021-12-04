289f51a566f98a9f00cb0d06835123ca515bc2d9
soonteckling <soonteckl@outlook.com>
2021-12-04 15:31:02 +0800

    added two signup behavior for companion and user

--- app/models/companion.rb
+++ app/models/companion.rb
@@ -1,8 +1,8 @@
 class Companion < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  validates :description, presence: true
-  validates :description, length: { minimum: 50,
+  # validates :description, presence: true
+  validates :description, length: { minimum: 10,
     too_long: "%{count} characters is the maximum allowed" }
   validates :price, numericality: { only_integer: true }
   validates :user_id, uniqueness: true
