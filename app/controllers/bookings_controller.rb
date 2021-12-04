c9b7e5ec9a39e93d83208a05e98ed2e3695d8752
Miguel Vela <rober2miguel@gmail.com>
2021-12-04 11:40:24 +0800

    Pushing my controller code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def new
+    @companion = Companion.find(params[:companion_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @companion = Companion.find(params[:companion_id])
+  end
+
+  def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date, :time, :price, :accepted)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :companion
   validates :date, presence: true
   validates :time, presence: true
+  validates :user_id, uniqueness: true
   validates :accepted, inclusion: [true, false]
   validates :user_id, uniqueness: { scope: :companion_id }
 end
