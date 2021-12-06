da8f045810a9982a36ae5b4da9889b506a139978
soonteckling <soonteckl@outlook.com>
2021-12-06 21:50:42 +0800

    fixed the registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-   stored_location_for(resource) || '/companions/new' # Or :prefix_to_your_route
+    '/companions/new' # Or :prefix_to_your_route
   end
 end
